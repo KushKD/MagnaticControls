b36fe39689d9f379e2d873b770d6462112fd593d
Kush <kush@Kushs-MacBook-Pro.local>
2015-03-07 11:18:23 +0530

    Initial commit

---
+++ uicontrols/src/main/java/com/kailash/magnet/uicontrols/BaseUIActivity.java
@@ -0,0 +1,15 @@
+package com.kailash.magnet.uicontrols;
+
+import android.app.Activity;
+
+/**
+ * Created by kush on 05/03/15.
+ */
+public class BaseUIActivity extends Activity {
+
+    public int add(int a , int b){
+
+        int c = a+b;
+        return c;
+    }
+}
---
+++ wear/src/main/java/com/kailash/magnet/magnaticcontrols/MainActivityWear.java
@@ -0,0 +1,27 @@
+package com.kailash.magnet.magnaticcontrols;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.kailash.magnet.uicontrols.BaseUIActivity;
+
+public class MainActivityWear extends BaseUIActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_wear);
+        mTextView = (TextView)findViewById(R.id.t1);
+
+
+        int x = add(34,132);
+        Log.d("The addition is",Integer.toString(x));
+        mTextView.setText(Integer.toString(x));
+
+    }
+}
