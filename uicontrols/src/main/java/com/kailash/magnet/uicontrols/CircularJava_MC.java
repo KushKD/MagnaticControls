3ab6653e0f02cd5dfd2eca22bc736f0c70d02c29
kush <kushkumardhawan@gmail.com>
2015-04-03 23:28:29 +0530

    This is the Fifth commit .
    The Program isn't working

--- uicontrols/src/main/java/com/kailash/magnet/uicontrols/CircularJava_MC.java
+++ uicontrols/src/main/java/com/kailash/magnet/uicontrols/CircularJava_MC.java
@@ -2,7 +2,12 @@ package com.kailash.magnet.uicontrols;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,6 +22,58 @@ import android.view.View;
  * ->
  */
 public class CircularJava_MC extends View {
+
+    /**
+     *
+     * Variables and Constants
+     * {@code Paint} instance used to draw the inactive circle.
+     * {@Path} instance used to draw the circle path
+     * {@code RectF} that represents the circle (or ellipse) of the seekbar.
+     */
+
+    private Paint OuterCirclePaint;
+    private Paint OuterCircleFillPaint;
+    private Path OuterCirclePath;
+    private RectF mCircleRectF = new RectF();
+
+    /**
+     * Default Color and Properties
+     * Holds the color value for {@code mCircleFillPaint} before the {@code Paint} instance is created.
+     */
+    private int mCircleFillColor = Color.BLUE;
+
+
+    /** Change the angle to create different shapes of the circle default is 270 270
+     *  Geometric (clockwise, relative to 3 o'clock)  default is 270
+     *  Geometric (clockwise, relative to 3 o'clock)
+     */
+    private static final float DEFAULT_START_ANGLE = 120f;
+    private static final float DEFAULT_END_ANGLE = 60f;
+
+
+    /**
+     * Start Angle and End Angle
+     * Start angle of the CircularSeekBar.
+     * Note: If mStartAngle and mEndAngle are set to the same angle, 0.1 is subtracted
+     * from the mEndAngle to make the circle function properly .
+     */
+    private float mStartAngle;
+    private float mEndAngle;
+
+    /**
+     * Width and the Height of the Circle
+     */
+    private float mCircleWidth;
+    private float mCircleHeight;
+
+    /**
+     * Total Degrees
+     *
+     */
+    private float mTotalCircleDegrees;
+
+
+
 
 
     public CircularJava_MC(Context context) {
@@ -65,6 +122,18 @@ public class CircularJava_MC extends View {
 
 
     /**
+     * Calculates the total degrees between mStartAngle and mEndAngle, and sets mTotalCircleDegrees
+     * to this value.
+     */
+    private void calculateTotalDegrees() {
+        mTotalCircleDegrees = (360f - (mStartAngle - mEndAngle)) % 360f; // Length of the entire circle/arc
+        if (mTotalCircleDegrees <= 0f) {
+            mTotalCircleDegrees = 360f;
+        }
+    }
+
+
+    /**
      * Actual Drawing begins here
      * Initializes the {@code Paint} objects with the appropriate styles.
      * Drawing of the circles , Rectangles and Paths goes here
@@ -73,7 +142,53 @@ public class CircularJava_MC extends View {
 
         //Optional Dash PAth Effect
         DashPathEffect dashPath = new DashPathEffect(new float[]{80,10}, (float)0.0 );
+        OuterCirclePaint = new Paint();
+        OuterCirclePaint.setAntiAlias(true);
+        OuterCirclePaint.setDither(true);
+        OuterCirclePaint.setColor(Color.GREEN);
+        OuterCirclePaint.setStrokeWidth(12);
+        OuterCirclePaint.setStyle(Paint.Style.STROKE);
+        OuterCirclePaint.setStrokeJoin(Paint.Join.ROUND);
+        OuterCirclePaint.setStrokeCap(Paint.Cap.ROUND);
+        OuterCirclePaint.setPathEffect(dashPath);
 
+        OuterCircleFillPaint = new Paint();
+        OuterCircleFillPaint.setAntiAlias(true);
+        OuterCircleFillPaint.setDither(true);
+        OuterCircleFillPaint.setColor(mCircleFillColor);
+        OuterCircleFillPaint.setStyle(Paint.Style.FILL);
+        OuterCircleFillPaint.setPathEffect(dashPath);
+
+    }
+
+
+    /**
+     * Initialize the {@code Path} objects with the appropriate values.
+     */
+    private void initPaths(){
+        OuterCirclePath = new Path();
+        OuterCirclePath.addArc(mCircleRectF, mStartAngle, mTotalCircleDegrees);
+    }
+
+    /**
+     *
+     */
+    private void initRects() {
+        mCircleRectF.set(-mCircleWidth, -mCircleHeight, mCircleWidth, mCircleHeight);
+    }
+
+    /**
+     * This is the main function of drawing stuff to the canvas.
+     * Takes canvas as the main parameter
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(this.getWidth() / 2, this.getHeight() / 2);
+        initRects();
+        calculateTotalDegrees();
+        initPaths();
+        canvas.drawPath(OuterCirclePath, OuterCirclePaint);
     }
 
 
--- wear/src/main/java/com/kailash/magnet/magnaticcontrols/MainActivityWear.java
+++ wear/src/main/java/com/kailash/magnet/magnaticcontrols/MainActivityWear.java
@@ -16,12 +16,12 @@ public class MainActivityWear extends BaseUIActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_wear);
-        mTextView = (TextView)findViewById(R.id.t1);
+       // mTextView = (TextView)findViewById(R.id.t1);
 
 
-        int x = add(34,132);
-        Log.d("The addition is",Integer.toString(x));
-        mTextView.setText(Integer.toString(x));
+       // int x = add(34,132);
+        //Log.d("The addition is",Integer.toString(x));
+       // mTextView.setText(Integer.toString(x));
 
         /**
          * Kush kumar Dhawan Working with Git
